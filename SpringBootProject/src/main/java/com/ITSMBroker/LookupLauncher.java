60df1a265f3e7d86dbf056626d9abd0ba06653f3
Joel Rizzo <milesprower1010@gmail.com>
2018-04-15 13:28:39 -0500

    Added OpenAM
    Added OpenAM

--- SpringBootProject/src/main/java/com/ITSMBroker/LookupLauncher.java
+++ SpringBootProject/src/main/java/com/ITSMBroker/LookupLauncher.java
@@ -5,33 +5,33 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class LookupLauncher implements CommandLineRunner {
-    @Autowired
-    private CustomerAccountRepository custRepo;
-    @Autowired
-    private OrganizationRepository orgRepo;
-    @Autowired
-    private ServiceRepository serviceRepo;
-    public static void main(String[] args) { SpringApplication.run(LookupLauncher.class, args); }
-    @Override
-    public void run(String... strings) throws Exception {
-		/*repository.deleteAll();
-		repository.save(new CustomerAccount("Walmart", "walmartuser", "walmartpass"));
-		repository.save(new CustomerAccount("Target", "targetuser", "targetpass"));
-		System.out.println("Customers found with findAll():");
-		System.out.println("-------------------------------");
-		for (CustomerAccount customer : (List<CustomerAccount>)repository.findAll())
-			System.out.println(customer);
-		System.out.println();
-		System.out.println("Customer found with findByCustomerName:");
-		System.out.println("--------------------------------");
-		System.out.println(repository.findByCustomerName("Walmart"));
-		System.out.println(repository.findByCustomerName("Target"));*/
-                /*Organization o = new Organization("Walmart");
-                orgRepo.save(o);
-                Service s = new Service("ServiceNow");
-                serviceRepo.save(s);
-                custRepo.save(new CustomerAccount(o, s, "admin", "passw", "stestu", "stestp", PermissionsLevel.ADMIN));*/
-    }
+//@SpringBootApplication
+public class LookupLauncher {//implements CommandLineRunner {
+//    @Autowired
+//    private CustomerAccountRepository custRepo;
+//    @Autowired
+//    private OrganizationRepository orgRepo;
+//    @Autowired
+//    private ServiceRepository serviceRepo;
+//    public static void main(String[] args) { SpringApplication.run(LookupLauncher.class, args); }
+//    @Override
+//    public void run(String... strings) throws Exception {
+//		/*repository.deleteAll();
+//		repository.save(new CustomerAccount("Walmart", "walmartuser", "walmartpass"));
+//		repository.save(new CustomerAccount("Target", "targetuser", "targetpass"));
+//		System.out.println("Customers found with findAll():");
+//		System.out.println("-------------------------------");
+//		for (CustomerAccount customer : (List<CustomerAccount>)repository.findAll())
+//			System.out.println(customer);
+//		System.out.println();
+//		System.out.println("Customer found with findByCustomerName:");
+//		System.out.println("--------------------------------");
+//		System.out.println(repository.findByCustomerName("Walmart"));
+//		System.out.println(repository.findByCustomerName("Target"));*/
+//                /*Organization o = new Organization("Walmart");
+//                orgRepo.save(o);
+//                Service s = new Service("ServiceNow");
+//                serviceRepo.save(s);
+//                custRepo.save(new CustomerAccount(o, s, "admin", "passw", "stestu", "stestp", PermissionsLevel.ADMIN));*/
+//    }
 }
